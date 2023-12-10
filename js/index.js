08d11457e79526ff027ee60cd35c2ee6100bd143
Hamza Khan Lodhi <hamzalodhi2023@gmail.com>
2023-12-10 13:31:18 +0500

    add slider

---
+++ js/index.js
@@ -0,0 +1,17 @@
+const imgs = document.querySelectorAll("#hero .img");
+const sliderImg = document.querySelector("#hero img.animation");
+let currentImgIndex = 0;
+
+function showImage() {
+  sliderImg.classList.remove("animation");
+
+  sliderImg.src = imgs[currentImgIndex].src;
+
+  sliderImg.offsetHeight;
+
+  sliderImg.classList.add("animation");
+
+  currentImgIndex = (currentImgIndex + 1) % imgs.length;
+}
+
+setInterval(showImage, 7000);
