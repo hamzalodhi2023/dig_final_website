cc4952eb9737c8a67d58e9b106fdddfa90039334
Hafsa Khan Lodhi <hafsalodhi2023@gmail.com>
2023-12-15 13:39:40 +0500

    created complaint.js

---
+++ js/complaint.js
@@ -0,0 +1,26 @@
+"use strict";
+/****************************************************************
+ * Hamburger Logic Start
+ ****************************************************************/
+
+let HamburgerIcon = document.querySelector(".fa-bars-staggered");
+let HamburgerCross = document.querySelector(".fa-xmark");
+let HamburgerContainer = document.querySelector(".hamburger-container");
+let HamburgerMenu = document.querySelectorAll(".hamburger-container ul a li");
+
+HamburgerIcon.addEventListener("click", HamburgerOpening);
+HamburgerCross.addEventListener("click", HamburgerClosing);
+HamburgerMenu.forEach((element) => {
+  element.addEventListener("click", HamburgerClosing);
+});
+
+function HamburgerOpening() {
+  HamburgerContainer.style.right = "0%";
+}
+
+function HamburgerClosing() {
+  HamburgerContainer.style.right = "-100%";
+}
+/****************************************************************
+ * Hamburger Logic End
+ ****************************************************************/
